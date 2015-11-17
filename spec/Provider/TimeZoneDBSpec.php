555a04e0b1929617dfc24c01315a31c3f57a0510
Michael Crumm <mike@crumm.net>
2015-11-17 10:04:00 -0800

    refactor(TimeZoneDB): Organize specs into contexts

--- spec/Provider/TimeZoneDBSpec.php
+++ spec/Provider/TimeZoneDBSpec.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Teazee\Spec;
+namespace Teazee\Spec\Provider;
 
 use Http\Client\HttpClient;
 use Teazee\Model\TimeZone;
@@ -17,10 +17,6 @@ describe (TimeZoneDB::class, function () {
         $this->lng = -88.3121289;
     });
 
-    after (function() {
-        VCR::eject();
-    });
-
     beforeEach (function() {
         $this->teazee = new TimeZoneDB('TVDB_TEAZEE_KEY');
     });
@@ -33,79 +29,81 @@ describe (TimeZoneDB::class, function () {
         expect($this->teazee->getClient())->toBeAnInstanceOf(HttpClient::class);
     });
 
-    it ('->find() returns a TimeZone', function () {
-        $tz = $this->teazee->find($this->lat, $this->lng);
+    context('with coordinates', function () {
 
-        expect ($tz)->toBeAnInstanceOf(TimeZone::class);
+        beforeEach (function () {
+           $this->tz = $this->teazee->find($this->lat, $this->lng);
+        });
+
+        it ('->find() returns a TimeZone', function () {
+            expect ($this->tz)->toBeAnInstanceOf(TimeZone::class);
+        });
+
+        it ('->find() has a DateTimeZone', function () {
+            expect ($this->tz->getDateTimeZone())->toBeAnInstanceOf(DateTimeZone::class);
+        });
+
+        it ('->find() has a name', function () {
+            expect ($this->tz->getName())->toBe('America/Chicago');
+        });
+
+        it ('->find() has a timestamp', function () {
+            expect ($this->tz->getTimestamp())->toBeAn('int');
+        });
+
+        it ('->find() has a utc offset', function () {
+            expect ($this->tz->getUtcOffset())->toBeAn('int');
+        });
+
+        it ('->find() has a timestamp in utc', function () {
+            expect ($this->tz->getTimestamp())->toBe(1447744964);
+        });
+
+        it ('->find() can check for daylight savings time', function () {
+            expect ($this->tz->isDst())->toBeFalsy();
+        });
     });
 
-    it ('->find() has a DateTimeZone', function () {
-        $tz = $this->teazee->find($this->lat, $this->lng);
+    context ('with coordinates and timestamp', function () {
+        it ('->find() can take an optional timestamp', function () {
+            $this->tz = $this->teazee->find($this->lat, $this->lng, 1446296400);
 
-        expect ($tz->getDateTimeZone())->toBeAnInstanceOf(DateTimeZone::class);
+            expect ($this->tz->isDst())->toBe(true);
+        });
     });
 
-    it ('->find() has a name', function () {
-        $tz = $this->teazee->find($this->lat, $this->lng);
+    context ('with invalid parameters', function () {
 
-        expect ($tz->getName())->toBe('America/Chicago');
+        it ('fails without an API key', function () {
+            $expected = function () {
+                $this->teazee = new TimeZoneDB('');
+                $this->teazee->find($this->lat, $this->lng);
+            };
+
+            expect ($expected)
+                ->toThrow(new RuntimeException('Parameter "key" (API Key) is not provided.'));
+        });
+
+        it ('fails with a bad API key', function () {
+            $expected = function () {
+                $this->teazee = new TimeZoneDB('TEAZEE_BAD_KEY');
+                $this->teazee->find($this->lat, $this->lng);
+            };
+
+            expect ($expected)
+                ->toThrow(new RuntimeException('Invalid API key.'));
+        });
+
+        it ('fails on invalid coordinates', function () {
+            $expected = function () {
+                $this->teazee->find('foo', 'bar');
+            };
+
+            expect ($expected)->toThrow(new RuntimeException('Invalid latitude value.'));
+        });
     });
 
-    it ('->find() has a timestamp', function () {
-        $tz = $this->teazee->find($this->lat, $this->lng);
-
-        expect ($tz->getTimestamp())->toBeAn('int');
-    });
-
-    it ('->find() has a utc offset', function () {
-        $tz = $this->teazee->find($this->lat, $this->lng);
-
-        expect ($tz->getUtcOffset())->toBeAn('int');
-    });
-
-    it ('->find() has a timestamp in utc', function () {
-        $tz = $this->teazee->find($this->lat, $this->lng);
-
-        expect ($tz->getTimestamp())->toBe(1447744964);
-    });
-
-    it ('->find() can check for daylight savings time', function () {
-        $tz = $this->teazee->find($this->lat, $this->lng);
-
-        expect ($tz->isDst())->toBe(false);
-    });
-
-    it ('->find() can take an optional timestamp', function () {
-        $tz = $this->teazee->find($this->lat, $this->lng, 1446296400);
-
-        expect ($tz->isDst())->toBe(true);
-    });
-
-    it ('fails without an API key', function () {
-        $expected = function () {
-            $this->teazee = new TimeZoneDB('');
-            $this->teazee->find($this->lat, $this->lng);
-        };
-
-        expect ($expected)
-            ->toThrow(new RuntimeException('Parameter "key" (API Key) is not provided.'));
-    });
-
-    it ('fails with a bad API key', function () {
-        $expected = function () {
-            $this->teazee = new TimeZoneDB('TEAZEE_BAD_KEY');
-            $this->teazee->find($this->lat, $this->lng);
-        };
-
-        expect ($expected)
-            ->toThrow(new RuntimeException('Invalid API key.'));
-    });
-
-    it ('fails on invalid params', function () {
-        $expected = function () {
-            $this->teazee->find('foo', 'bar');
-        };
-
-        expect ($expected)->toThrow(new RuntimeException('Invalid latitude value.'));
+    after (function() {
+        VCR::eject();
     });
 });
