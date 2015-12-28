ff52c24ef06ba000c122c81e83e4bb4b7698e108
Michael Crumm <mike@crumm.net>
2015-12-27 20:21:19 -0800

    chore: Bump version to 0.2.2-dev

--- src/Teazee.php
+++ src/Teazee.php
@@ -19,7 +19,7 @@ interface Teazee
     /**
      * Version.
      */
-    const VERSION = '0.2.1';
+    const VERSION = '0.2.2-dev';
 
     /**
      * Finds a TimeZone for a location on the surface of the earth.
